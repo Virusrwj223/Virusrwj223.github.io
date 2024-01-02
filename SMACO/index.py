cef669691ae048ffe0de91f73759fc9d49fbdbe6
Virusrwj223 <129716446+Virusrwj223@users.noreply.github.com>
2024-01-02 14:16:49 +0800

    Delete SMACO directory

--- SMACO/index.py
+++
@@ -1,8 +0,0 @@
-from flask import Blueprint, render_template
-
-index = Blueprint(__name__,"index")
-
-@index.route("/")
-
-def home():
-    return render_template("index.html", routing="/calculator/")
